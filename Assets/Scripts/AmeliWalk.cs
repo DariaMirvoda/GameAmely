999cbac8f5a2c3d63bedb2fa790bd309207c21d0
DariaMirvoda <mirvoda.dasha@yandex.ru>
2018-05-21 12:04:26 +0500

    first commit

---
+++ Assets/Scripts/AmeliWalk.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AmeliWalk : MonoBehaviour
+{
+
+    private Animator anim;
+
+    private bool isWalking;
+    private float walkSpeed, walkTime, distance;
+    private Vector3 pointA, pointB;
+    private Action onWalkEnd;
+
+    private bool isTurning;
+    private float turnSpeed, turnProgress;
+    private Quaternion rotationA, rotationB;
+    private Action onTurnEnd;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (isWalking)
+        {
+            float totalTime = distance / walkSpeed;
+            float progress = walkTime / totalTime;
+            transform.position = Vector3.Lerp(pointA, pointB, progress);
+            walkTime += Time.deltaTime;
+            if (progress >= 1)
+            {
+                isWalking = false;
+                anim.SetBool("IsWalking", false);
+                if (onWalkEnd != null)
+                {
+                    onWalkEnd();
+                }
+            }
+        }
+
+
+        if (isTurning)
+        {
+            transform.localRotation = Quaternion.Lerp(rotationA, rotationB, turnProgress);
+            turnProgress += Time.deltaTime * turnSpeed;
+            if (turnProgress >= 1)
+            {
+                isTurning = false;
+                if (onTurnEnd != null)
+                {
+                    onTurnEnd();
+                }
+            }
+        }
+    }
+
+
+    public void StartWalking(Vector3 pointA, Vector3 pointB, float speed, Action onWalkEnd)
+    {
+        isWalking = true;
+        walkTime = 0;
+        walkSpeed = speed;
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.onWalkEnd = onWalkEnd;
+        distance = Vector3.Distance(pointA, pointB);
+        anim.SetBool("IsWalking", true);
+    }
+
+
+    public void StartRotating(Quaternion rotationA, Quaternion rotationB, float speed, Action onTurnEnd)
+    {
+        isTurning = true;
+        turnProgress = 0;
+        turnSpeed = speed;
+        this.rotationA = rotationA;
+        this.rotationB = rotationB;
+        this.onTurnEnd = onTurnEnd;
+    }
+}
---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+
+    public AmeliWalk AmeliWalk;
+    public Transform LeftPosReference, RightPosReference, StopPosReference;
+    public GameObject MenuGameObject, GameText;
+
+
+    public void MakeTransition()
+    {
+        Vector3 rightPoint = new Vector3(RightPosReference.position.x, AmeliWalk.transform.position.y, AmeliWalk.transform.position.z);
+        Vector3 walkDirection = rightPoint - AmeliWalk.transform.position;
+        walkDirection.y = 0;
+        Quaternion walkRotation = Quaternion.LookRotation(walkDirection);
+        AmeliWalk.StartRotating(AmeliWalk.transform.localRotation, walkRotation, 5, null);
+        AmeliWalk.StartWalking(AmeliWalk.transform.position, rightPoint, 5, () =>
+        {
+            MenuGameObject.SetActive(false);
+            GameText.SetActive(true);
+            Vector3 leftPoint = new Vector3(LeftPosReference.position.x, AmeliWalk.transform.position.y, AmeliWalk.transform.position.z);
+            Vector3 stopPoint = new Vector3(StopPosReference.position.x, AmeliWalk.transform.position.y, AmeliWalk.transform.position.z);
+            AmeliWalk.StartWalking(leftPoint, stopPoint, 5, () =>
+            {
+                AmeliWalk.StartRotating(AmeliWalk.transform.localRotation, Quaternion.Euler(0, 160, 0), 5, null);
+            });
+        });
+    }
+}
